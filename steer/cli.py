5a3075014ee59a0cc8ae3856b5a1b74e512a0e46
José Coelho <16445494+jcoelho93@users.noreply.github.com>
2024-06-02 13:21:11 +0100

    Add first iteration of save

--- steer/cli.py
+++ steer/cli.py
@@ -3,7 +3,6 @@ import click
 import questionary
 from io import TextIOWrapper
 from steer.models import Schema
-from questionary import prompt
 
 
 @click.group()
@@ -13,12 +12,8 @@ def cli():
 
 @cli.command(help='Interactively build a values.yml file')
 @click.argument('json_schema', type=click.File('r'))
-def values(json_schema: TextIOWrapper):
+@click.option('--output-file', '-o', help="The file to output data")
+def values(json_schema: TextIOWrapper, output_file: str = None):
     content = json.loads(json_schema.read())
     schema = Schema.from_dict(content)
-
-    while True:
-        property = questionary.select('Select a property', choices=[p.name for p in schema.properties]).ask()
-        for p in schema.properties:
-            if p.name == property:
-                p.prompt()
+    schema.prompt(output_file)
--- steer/models.py
+++ steer/models.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel
-from typing import Optional, List
+import questionary
+from io import TextIOWrapper
 from questionary import prompt
+from jsonpath_ng import jsonpath, parse
+from pydantic import BaseModel
+from typing import Optional, List, Any, Dict
 
 
 class Property(BaseModel):
@@ -8,6 +11,23 @@ class Property(BaseModel):
     ref: Optional[str] = None
     name: Optional[str] = None
     description: Optional[str] = None
+    allOf: Optional[List[BaseModel]] = None
+    path: Optional[str] = None
+    value: Optional[Any] = None
+
+    @classmethod
+    def from_dict(cls, key, obj):
+        return cls(
+            name=key,
+            type=obj.get('type'),
+            ref=obj.get('$ref'),
+            description=obj.get('description'),
+            allOf=obj.get('allOf')
+        )
+
+    def save(self, fp: TextIOWrapper):
+        if self.value:
+            fp.write('{} = {}\n'.format(self.path, self.value))
 
 
 class StringProperty(Property):
@@ -15,31 +35,34 @@ class StringProperty(Property):
     format: Optional[str] = None
     default: Optional[str] = None
     enum: Optional[List[str]] = None
+    pattern: Optional[str] = None
 
     @classmethod
-    def from_dict(cls, key, obj):
+    def from_dict(cls, key, obj, parent):
         return cls(
             name=key,
             type=obj.get('type'),
             format=obj.get('format'),
             default=obj.get('default'),
-            enum=obj.get('enum')
+            enum=obj.get('enum'),
+            pattern=obj.get('pattern'),
+            path=parent + key
         )
 
     def prompt(self):
         if self.enum:
-            return prompt({
+            self.value = prompt({
                 'type': 'select',
                 'name': self.name,
                 'message': f"{self.name} (string): [{self.default or ""}]",
                 'choices': self.enum
-            })
+            }).get(self.name)
         else:
-            return prompt({
+            self.value = prompt({
                 'type': 'input',
                 'name': self.name,
                 'message': f"{self.name} (string): [{self.default or ""}]",
-            })
+            }).get(self.name)
 
 
 class IntegerProperty(Property):
@@ -49,21 +72,30 @@ class IntegerProperty(Property):
     enum: Optional[List[int]] = None
 
     @classmethod
-    def from_dict(cls, key, obj):
+    def from_dict(cls, key, obj, parent):
         return cls(
             name=key,
             type=obj.get('type'),
             format=obj.get('format'),
             default=obj.get('default'),
-            enum=obj.get('enum')
+            enum=obj.get('enum'),
+            path=parent + key
         )
 
     def prompt(self):
-        return prompt({
-            'type': 'input',
-            'name': self.name,
-            'message': f"{self.name} (int): [{self.default or ""}]",
-        })
+        if self.enum:
+            self.value = prompt({
+                'type': 'select',
+                'name': self.name,
+                'message': f"{self.name} (int): [{self.default or ""}]",
+                'choices': self.enum
+            }).get(self.name)
+        else:
+            self.value = prompt({
+                'type': 'input',
+                'name': self.name,
+                'message': f"{self.name} (int): [{self.default or ""}]",
+            }).get(self.name)
 
 
 class ObjectProperty(Property):
@@ -72,33 +104,49 @@ class ObjectProperty(Property):
     properties: Optional[List[Property]] = []
 
     @classmethod
-    def from_dict(cls, key, obj):
+    def from_dict(cls, key, obj, parent):
         property = cls(
             name=key,
-            type=obj.get('type'),
+            type=obj.get('type', 'object'),
             additionalProperties=bool(obj.get('additionalProperties', False)),
+            path=parent + key
         )
         if obj.get('properties') is not None:
-            return property.with_properties(obj['properties'])
+            return property.with_properties(obj['properties'], parent + key)
 
         return property
 
     def add_property(self, property: Property):
         self.properties.append(property)
 
-    def with_properties(self, properties):
+    def with_properties(self, properties, parent):
         for key, value in properties.items():
             if value.get('type') == 'string':
-                self.add_property(StringProperty.from_dict(key, value))
+                self.add_property(StringProperty.from_dict(key, value, parent + '.'))
             elif value.get('type') == 'integer':
-                self.add_property(IntegerProperty.from_dict(key, value))
+                self.add_property(IntegerProperty.from_dict(key, value, parent + '.'))
             elif value.get('type') == 'object':
-                self.add_property(ObjectProperty.from_dict(key, value))
+                self.add_property(ObjectProperty.from_dict(key, value, parent + '.'))
             elif value.get('type') == 'boolean':
-                self.add_property(BooleanProperty.from_dict(key, value))
+                self.add_property(BooleanProperty.from_dict(key, value, parent + '.'))
             elif value.get('type') == 'array':
-                self.add_property(ArrayProperty.from_dict(key, value))
+                self.add_property(ArrayProperty.from_dict(key, value, parent + '.'))
         return self
+
+    def save(self, fp: TextIOWrapper):
+        for property in self.properties:
+            property.save(fp)
+
+    def prompt(self):
+        while True:
+            choices = [p.name for p in self.properties]
+            choices.append('Back')
+            property = questionary.select('Select a property', choices=choices).ask()
+            if property == 'Back':
+                return
+            for p in self.properties:
+                if p.name == property:
+                    p.prompt()
 
 
 class BooleanProperty(Property):
@@ -106,19 +154,20 @@ class BooleanProperty(Property):
     default: Optional[bool] = False
 
     @classmethod
-    def from_dict(cls, key, obj):
+    def from_dict(cls, key, obj, parent):
         return cls(
             name=key,
             type=obj.get('type'),
-            default=obj.get('default')
+            default=obj.get('default'),
+            path=parent + key
         )
 
     def prompt(self):
-        return prompt({
+        self.value = prompt({
             'type': 'confirm',
             'name': self.name,
             'message': f"{self.name}: [{self.default or False}]",
-        })
+        }).get(self.name)
 
 
 class ArrayProperty(Property):
@@ -126,19 +175,24 @@ class ArrayProperty(Property):
     items: Optional[Property] = None
 
     @classmethod
-    def from_dict(cls, key, obj):
+    def from_dict(cls, key, obj, parent):
         return cls(
             name=key,
             type=obj.get('type'),
-            items=obj.get('items')
+            items=obj.get('items'),
+            path=parent + key
         )
 
     def prompt(self):
-        return prompt({
+        self.value = prompt({
             'type': 'input',
             'name': self.name,
             'message': f"{self.name} (array):",
-        })
+        }).get(self.name)
+
+
+class RefProperty(Property):
+    type: str = 'ref'
 
 
 class Schema(BaseModel):
@@ -149,9 +203,35 @@ class Schema(BaseModel):
     required: Optional[List[str]] = []
     properties: Optional[List[Property]] = []
     definitions: Optional[List[Property]] = []
+    data: Dict[str, Any] = {}
 
     def add_property(self, property: Property):
         self.properties.append(property)
+
+    def add_definition(self, definition: Property):
+        self.definitions.append(definition)
+
+    def save(self, filepath):
+        with open(filepath, 'w') as fp:
+            for property in self.properties:
+                property.save(fp)
+
+    def prompt(self, output_file: str = None):
+        try:
+            while True:
+                choices = [p.name for p in self.properties]
+                choices.append('Save')
+                choices.append('Exit')
+                property = questionary.select('Select a property', choices=choices).ask()
+                if property == 'Exit':
+                    exit(0)
+                if property == 'Save':
+                    self.save(output_file)
+                for p in self.properties:
+                    if p.name == property:
+                        p.prompt()
+        except KeyboardInterrupt:
+            exit(0)
 
     @classmethod
     def from_dict(cls, obj):
@@ -163,16 +243,21 @@ class Schema(BaseModel):
             required=obj.get('required'),
         )
 
+        for key, value in obj.get('definitions', []).items():
+            schema.add_definition(ObjectProperty.from_dict(key, value, '$.'))
+
         for key, value in obj['properties'].items():
             if value.get('type') == 'string':
-                schema.add_property(StringProperty.from_dict(key, value))
+                schema.add_property(StringProperty.from_dict(key, value, '$.'))
             elif value.get('type') == 'integer':
-                schema.add_property(IntegerProperty.from_dict(key, value))
+                schema.add_property(IntegerProperty.from_dict(key, value, '$.'))
             elif value.get('type') == 'object':
-                schema.add_property(ObjectProperty.from_dict(key, value))
+                schema.add_property(ObjectProperty.from_dict(key, value, '$.'))
             elif value.get('type') == 'boolean':
-                schema.add_property(BooleanProperty.from_dict(key, value))
+                schema.add_property(BooleanProperty.from_dict(key, value, '$.'))
             elif value.get('type') == 'array':
-                schema.add_property(ArrayProperty.from_dict(key, value))
+                schema.add_property(ArrayProperty.from_dict(key, value, '$.'))
+            elif value.get('type') == 'ref':
+                schema.add_property(RefProperty.from_dict(key, value, '$.'))
 
         return schema
